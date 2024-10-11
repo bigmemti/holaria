8ae90b2b87288293788c207a83758df835312f82
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-10-11 20:44:46 +0330

    add simple nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts"></script>
 
 <template lang="pug">
-  div 
-    ColorModeButton
-    UButton(to="/") home
+  div
+    header
+      nav(class="flex justify-between")
+        ColorModeButton
+        ul(class="flex")
+          li 
+            UButton(to="/" variant='guest' size="xl") home
+          li 
+            UButton(to="/" variant='guest' size="xl") login
+          li 
+            UButton(to="/" variant='guest' size="xl") sign up
+          li 
+            UButton(to="/" variant='guest' size="xl") dashboard
     slot
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+
+</style>
